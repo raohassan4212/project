3cbe02a7b2db681cf4440432ca8ac64229da4ace
raohassan4212 <52700173+raohassan4212@users.noreply.github.com>
2023-04-08 15:11:54 +0500

    project complete

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+{/* <a href="" class="navbar-brand p-0">
+                    <h1 class="text-primary m-0"><i class="fa fa-utensils me-3"></i>Restoran</h1>
+                    <!-- <img src="img/logo.png" alt="Logo"> -->
+                </a> */}
